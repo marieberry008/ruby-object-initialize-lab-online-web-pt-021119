92aee471698e5a12cc707e3cbc3b0ecdfe635227
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-03 23:59:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,6 @@
-class Dog 
-  def initialize(name, breed=Mutt)
+class Dog
+  def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
   end
 end
-
-fido = Dog.new("Mutt")
-fido.breed
