bcc89009ecb101f8e587c08d11b9ebaf9f94fbd3
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-03 23:56:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,5 +5,9 @@ class Dog
   end
 
 fido = Dog.new("Fido", "Mutt")
+
+fido = Dog.new("Fido")
+#Its attribute of its breed "Collie" will now be passed into .initialize(breed) as the breed argument. 
+fido.breed #=> "Collie"
   
 end
